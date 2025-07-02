8914aca0fd525766a4d73b4d76d0e985c4e50097
Sun <xy.sunviv@gmail.com>
2025-07-02 14:52:29 +0800

    feat: 示例页面修改

--- src/pages/playground/index.vue
+++ src/pages/playground/index.vue
@@ -1,9 +1,13 @@
 <template>
   <view class="h-full flex flex-col overflow-hidden">
-    <view class="mx-3 mt-10 rounded-md bg-white p-3">
-      <view class="flex items-center justify-between text-34 text-font-color-primary font-medium">
-        <view>T23120100025(越库)</view>
-        <view>徐运正</view>
+    <view class="m-3 rounded-lg bg-white p-3">
+      <view class="flex flex-row items-center justify-between text-34 text-font-color-primary font-medium">
+        <view class="mr-3 break-all">
+          T23120100025(越库)
+        </view>
+        <view class="flex-shrink-0">
+          徐运正
+        </view>
       </view>
       <view class="mt-3 flex justify-between rounded-md bg-placeholder p-3">
         <view class="h-112 flex flex-col justify-between">
